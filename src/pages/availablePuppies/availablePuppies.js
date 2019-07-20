d52f9594dbc197fd0cbb97279bfcee2e40a59f0d
Rebecca Potter <rebeccapotter@Rebeccas-MacBook-Air.local>
2019-07-19 18:33:44 -0600

    availablepuppies

--- src/pages/availablePuppies/availablePuppies.js
+++ src/pages/availablePuppies/availablePuppies.js
@@ -3,12 +3,45 @@ import './availablePuppies.css';
 import Navbar from '../../components/navBar/navBar';
 import Footer from '../../components/footer/footer';
 import Banner from '../../assets/images/ui/AvailablePuppiesBanner.png';
+import Percy from '../../assets/images/ui/Percy.jpeg';
+import Fleur from '../../assets/images/ui/Fleur.jpeg';
+import Serius from '../../assets/images/ui/Sirus.JPG';
+import Albus from '../../assets/images/ui/AlbusPuppy.jpg';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image';
+
 
 const AvailablePuppies = props => (
     <div>
    
         <div>
             <img className="banner" src={Banner} alt="banner"/>
+        </div>
+        <div className="availableHeader">Available Puppies!</div>
+        <div className="availableBody">The Puppies below are ready to go to a new home!</div>
+        <div className="availablePuppy">
+        <div className="row">
+             <div className="col-xs">
+                <img className="image" src={Percy} alt="percy" rounded />
+                <p className="availableBody"> Percy $1800 </p>
+            </div>
+            <div className="col-xs">
+                <img className="image" src={Fleur} alt="fleur" rounded />
+                <p className="availableBody"> Fleur $1800 </p>
+
+            </div>
+            <div className="col-xs">
+                <img className="image" src={Albus} alt="albus" rounded />
+                <p className="availableBody"> Albus $1800 </p>
+
+            </div>
+            <div className="col-xs">
+                <img className="image" src={Serius} alt="serius" rounded />
+                <p className="availableBody"> Serius $1800 </p>
+
+            </div>
+        </div>
         </div>
         <div>
             <Footer />

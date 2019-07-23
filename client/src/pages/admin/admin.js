e83650ee3b99574bb47a06def139648f5f4264b3
BenShepherd <19ute14@gmail.com>
2019-07-23 14:34:10 -0600

    admin login form

--- client/src/pages/admin/admin.js
+++ client/src/pages/admin/admin.js
@@ -1,15 +1,44 @@
 import React from 'react';
 import './admin.css';
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+
 
 
 const Admin = props => (
     <div>
         <div className="adminHeader">Admin Page</div>
-    
-   
-    
-</div>
+        <div className="login">Please Log In</div>
+        <Form>
+            <Form.Group controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" />
+                <Form.Text className="text-muted">
+
+                </Form.Text>
+            </Form.Group>
+
+            <Form.Group controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Form.Group controlId="formBasicChecbox">
+                <Form.Check type="checkbox" label="Keep Me Signed In" />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+                Login
+
+            </Button>
+        </Form>
+
+
+    </div>
 
 );
+
+
+
+
+
 
 export default Admin;
